a6e9d951eba5de8b6e7262009da817b3d72f2fe7
alihassan1177 <ah927481@gmail.com>
2023-04-28 16:49:15 +0500

    created auth pages

--- templates/client/auth/register.php
+++ templates/client/auth/register.php
@@ -14,10 +14,10 @@
 <!-- Contact Start -->
 <div class="container-fluid pt-5">
     <div class="text-center mb-4">
-        <h2 class="section-title px-5"><span class="px-2">Contact For Any Queries</span></h2>
+        <h2 class="section-title px-5"><span class="px-2">Create new Account</span></h2>
     </div>
     <div class="row px-xl-5">
-        <div class="col-lg-7 mb-5">
+        <div class="col-lg-6 mx-auto mb-5">
             <div class="contact-form">
                 <div id="success"></div>
                 <form name="sentMessage" id="contactForm" novalidate="novalidate">
@@ -30,36 +30,26 @@
                         <p class="help-block text-danger"></p>
                     </div>
                     <div class="control-group">
-                        <input type="text" class="form-control" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                        <input type="password" class="form-control" id="password" placeholder="Password" required="required" data-validation-required-message="Please enter a password" />
                         <p class="help-block text-danger"></p>
                     </div>
                     <div class="control-group">
-                        <textarea class="form-control" rows="6" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                        <input type="text" class="form-control" id="address" placeholder="Address" required="required" data-validation-required-message="Please enter a Address" />
+                        <p class="help-block text-danger"></p>
+                    </div>
+                    <div class="control-group">
+                        <input type="text" class="form-control" id="phone" placeholder="Phone No" required="required" data-validation-required-message="Please enter a Phone Number" />
                         <p class="help-block text-danger"></p>
                     </div>
                     <div>
-                        <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">Send
-                            Message</button>
+                        <button class="btn btn-primary py-2 px-4" type="submit" id="sendMessageButton">Register</button>
                     </div>
                 </form>
+                <p class="mt-3">Already have an Account? <a href="/login">Login Here</a></p>
+
             </div>
         </div>
-        <div class="col-lg-5 mb-5">
-            <h5 class="font-weight-semi-bold mb-3">Get In Touch</h5>
-            <p>Justo sed diam ut sed amet duo amet lorem amet stet sea ipsum, sed duo amet et. Est elitr dolor elitr erat sit sit. Dolor diam et erat clita ipsum justo sed.</p>
-            <div class="d-flex flex-column mb-3">
-                <h5 class="font-weight-semi-bold mb-3">Store 1</h5>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                <p class="mb-2"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
-            </div>
-            <div class="d-flex flex-column">
-                <h5 class="font-weight-semi-bold mb-3">Store 2</h5>
-                <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-                <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-                <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
-            </div>
-        </div>
+        
     </div>
 </div>
 <!-- Contact End -->
