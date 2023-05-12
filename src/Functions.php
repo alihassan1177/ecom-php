2bf08354e51c3027744896681c24edfcdc9f5e5d
alihassan1177 <ah927481@gmail.com>
2023-05-12 23:08:45 +0500

    ProductComponent product image

--- src/Functions.php
+++ src/Functions.php
@@ -112,16 +112,18 @@ class Functions
     echo "</ol></nav>";
   }
 
-  public static function ProductComponent(array $product, string $placeholderImage = ""){
+  public static function ProductComponent(array $product, string $placeholderImage = "")
+  {
     $productName = $product["name"];
     $productPrice = $product["price"];
     $productID = $product["id"];
-    
+    $productImage = $product["image"] != "" ? $product["image"]  : $placeholderImage;
+
     echo <<<HTML
      <div class="col-lg-3 col-md-6 col-sm-12 pb-1">
           <div class="card product-item border-0 mb-4">
             <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-              <img class="img-fluid w-100" src="$placeholderImage" alt="">
+              <img class="img-fluid w-100" src="$productImage" alt="">
             </div>
             <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
               <h6 class="text-truncate mb-3">$productName</h6>
