1d42b3db9bdcc67107d3d20e3449c25658385348
alihassan1177 <ah927481@gmail.com>
2023-05-16 12:58:46 +0500

    database error exceptions

--- src/core/Database.php
+++ src/core/Database.php
@@ -8,18 +8,30 @@ class Database
 
     public static function connect()
     {
-        self::$conn = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+        $connection = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+        if (!$connection) {
+            throw new \Exception("DATABASE CONNECTION FAILED, CHECK SERVER", 1);
+            return;
+        }
+        self::$conn = $connection;
     }
 
     public static function getResultsByQuery(string $query)
     {
-        //echo $query;
         $result = mysqli_query(self::$conn, $query);
+        if (!$result) {
+            throw new \Exception("DATABASE GET RESULTS QUERY FAILED \nSQL : $query", 1);
+            return;
+        }
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 
     public static function onlyExecuteQuery(string $query)
     {
-        mysqli_query(self::$conn, $query);
+        $result = mysqli_query(self::$conn, $query);
+        if (!$result) {
+            throw new \Exception("DATABASE GET RESULTS QUERY FAILED \nSQL : $query", 1);
+            return;
+        }
     }
 }
